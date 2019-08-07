98ed310edf797227f0f2964651cf467ef899ed63
Cullan Shewfelt <cullanrocks@gmail.com>
2019-08-07 06:59:24 -0700

    Init

---
+++ NexTravel.js
@@ -0,0 +1,11 @@
+const doSomething = (arguement) => {
+  for(let dx= -1; dx < 1; ++dx){
+    console.log(dx)
+  }
+}
+
+let answer = doSomething('do something');
+
+
+
+// https://github.com/sasZilla/cellular-automata-program/blob/master/main.js
